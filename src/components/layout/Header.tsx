f6ffa55803a3ad38d957de19c11084c71f1880fe
pablodanbarajas <pablo.lara6279@alumnos.udg.mx>
2026-03-06 13:58:11 -0600

    Fix: Remove unused Button import from Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,3 @@
-import { Button } from '@/components/ui/Button'
-
 export const Header = () => {
   return (
     <header className="fixed top-0 right-0 left-64 h-16 bg-[#f8f8f8] border-b border-[#504840] px-6 flex items-center justify-between lg:left-64">
