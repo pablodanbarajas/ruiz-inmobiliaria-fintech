326c653490c82a2017edc72790952540fbdf28cf
pablodanbarajas <pablo.lara6279@alumnos.udg.mx>
2026-07-17 17:30:26 -0600

    fix: correct layout by removing unnecessary closing div in documentos modal

--- src/pages/admin/Traspasos.tsx
+++ src/pages/admin/Traspasos.tsx
@@ -548,7 +548,7 @@ export const Traspasos = () => {
             </>
           )}
         </div>
-      </div>
+
         {/* Documentos de traspaso modal */}
         {showDocsModal && docsTraspaso && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4">
